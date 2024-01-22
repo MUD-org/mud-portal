2b79e20df4d72137c1212883ee8facadc864a0a0
biscuitWizard <1904063+biscuitWizard@users.noreply.github.com>
2024-01-21 22:23:57 -0800

    disabling darkmode theme

--- apps/frontend/src/theme.tsx
+++ apps/frontend/src/theme.tsx
@@ -3,20 +3,20 @@ import { red } from '@mui/material/colors';
 
 // A custom theme for this app
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#317873',
-    },
-    secondary: {
-      main: '#f50057',
-    },
-    text: {
-      primary: '#D3d3d3',
-    },
-    background: {
-      default: '#1c1c1c',
-    },
-  },
+  // palette: {
+  //   primary: {
+  //     main: '#317873',
+  //   },
+  //   secondary: {
+  //     main: '#f50057',
+  //   },
+  //   text: {
+  //     primary: '#D3d3d3',
+  //   },
+  //   background: {
+  //     default: '#1c1c1c',
+  //   },
+  // },
 });
 
 export default theme;
